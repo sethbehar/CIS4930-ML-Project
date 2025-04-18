10d40ecd1d1e71d55827e48ef4db41f67664374f
sethbehar <sethbehar@gmail.com>
2025-04-18 10:06:12 -0400

    merge changes

--- models/randomForest.py
+++ models/randomForest.py
@@ -8,6 +8,5 @@ class randomForest():
     def train(self, X_train, y_train):
         self.model.fit(X_train, y_train)
 
-    def predict(self, X_test, y_test):
-        pred = self.model.predict(X_test)
-        return evaluate_model(y_test, pred)
+    def predict(self, X_test):
+            return self.model.predict(X_test)
